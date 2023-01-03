c7c45e776f048854f31e311a02b54aabe81a11bd
Michael Howard (CYBER SECURITY) <mikehow@ntdev.microsoft.com>
2023-01-03 11:26:33 -0600

    Major update - now does multiple privs in one go. Better error handling.

--- LestPriv/LestPriv.cpp
+++ LestPriv/LestPriv.cpp
@@ -1,63 +1,82 @@
 #include <windows.h>
 #include <sddl.h>
+#include <string>
+#include <vector>
 #include "sal.h"
 
 #pragma comment(lib, "advapi32.lib")
 
-bool RemovePrivilege(_In_z_ LPCWSTR privilege) {
+_Check_return_ bool RemovePrivileges(_In_ const std::vector<std::wstring> privsToRemove)
+{
+    if (privsToRemove.size() == 0)
+        return false;
+	
     HANDLE hToken{};
-	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &hToken)) {
-		return false;
-	}
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
+        return false;
 
-    LUID luid{};
-	if (!LookupPrivilegeValue(NULL, privilege, &luid)) {
-		CloseHandle(hToken);
-		return false;
-	}   
+    TOKEN_PRIVILEGES* pTokenPrivs = (TOKEN_PRIVILEGES*)malloc(sizeof(TOKEN_PRIVILEGES) + sizeof(LUID_AND_ATTRIBUTES) * privsToRemove.size());
+    if (!pTokenPrivs) return false;
+	
+    pTokenPrivs->PrivilegeCount = privsToRemove.size();
 
-    TOKEN_PRIVILEGES tp{};
-    tp.PrivilegeCount = 1;
-    tp.Privileges[0].Luid = luid;
-    tp.Privileges[0].Attributes = SE_PRIVILEGE_REMOVED;
+    bool fRes = true;
 
-	if (!AdjustTokenPrivileges(hToken, false, &tp, 0, NULL, NULL)) {
-		CloseHandle(hToken);
-		return false;
-	}
+    for (int i = 0; i < privsToRemove.size(); i++)
+    {
+        if (!LookupPrivilegeValue(NULL, privsToRemove[i].c_str(), &pTokenPrivs->Privileges[i].Luid))
+        {
+            fRes = false;
+            break;
+        }
 
-    return true;
+        pTokenPrivs->Privileges[i].Attributes = SE_PRIVILEGE_REMOVED;
+    }
+
+	if (!fRes || !AdjustTokenPrivileges(hToken, FALSE, pTokenPrivs, sizeof(TOKEN_PRIVILEGES) + sizeof(LUID_AND_ATTRIBUTES) * privsToRemove.size(), NULL, NULL))
+		fRes = false;
+
+    if (pTokenPrivs) free(pTokenPrivs);
+    if (hToken) CloseHandle(hToken);
+
+    return fRes;
 }
 
-bool SetLowIntegrityLevel() {
+_Check_return_ bool SetLowIntegrityLevel() {
     HANDLE hToken{};
-	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &hToken)) {
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &hToken)) 
 		return false;
-	}
 
     TOKEN_MANDATORY_LABEL integrityLabel{};
-    PSID pSidIL = NULL;
-    ConvertStringSidToSid(L"S-1-16-4096", &pSidIL);
+    PSID pSidIntgrityLabel = NULL;
+    ConvertStringSidToSid(L"S-1-16-4096", &pSidIntgrityLabel);
     integrityLabel.Label.Attributes = SE_GROUP_INTEGRITY | SE_GROUP_INTEGRITY_ENABLED;
-    integrityLabel.Label.Sid = pSidIL;
+    integrityLabel.Label.Sid = pSidIntgrityLabel;
 
+    bool fRes = true;
     DWORD dwTokenSize = 0;
-	if (!GetTokenInformation(hToken, TokenIntegrityLevel, (LPVOID)&integrityLabel, dwTokenSize, &dwTokenSize) && GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
-        if (pSidIL) free(pSidIL);
-        return false;
-    }
-	
-    if (!SetTokenInformation(hToken, TokenIntegrityLevel, (LPVOID)&integrityLabel, dwTokenSize)) {
-        if (pSidIL) free(pSidIL);
-        return false;
-    }
-	
-    if (pSidIL) free(pSidIL);
+	if (!GetTokenInformation(hToken, TokenIntegrityLevel, (LPVOID)&integrityLabel, dwTokenSize, &dwTokenSize) && GetLastError() != ERROR_INSUFFICIENT_BUFFER) 
+        fRes = false;
+    
+    if (!fRes|| !SetTokenInformation(hToken, TokenIntegrityLevel, (LPVOID)&integrityLabel, dwTokenSize)) 
+        fRes = false;
+    
+    if (pSidIntgrityLabel) free(pSidIntgrityLabel);
 
-    return true;
+    return fRes;
 }
 
-void main() {
-    RemovePrivilege(SE_SHUTDOWN_NAME);
-    SetLowIntegrityLevel();
+int main() {
+
+    // From https://learn.microsoft.com/en-us/windows/win32/secauthz/privilege-constants
+    const std::vector<std::wstring> privs{ SE_BACKUP_NAME, SE_RESTORE_NAME, SE_TCB_NAME, SE_TAKE_OWNERSHIP_NAME, 
+                                           SE_DEBUG_NAME, SE_IMPERSONATE_NAME,  SE_CREATE_GLOBAL_NAME, SE_CREATE_TOKEN_NAME, 
+                                           SE_SECURITY_NAME, SE_RELABEL_NAME, SE_LOAD_DRIVER_NAME, SE_SYSTEMTIME_NAME };
+
+    if (RemovePrivileges(privs) == false || SetLowIntegrityLevel() == false) {
+        printf("Failed to remove privileges or set low integrity level");
+        return false;
+    }
+
+    return true;
 }
