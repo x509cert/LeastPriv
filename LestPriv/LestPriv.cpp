8d186c1a08355929bba201687665e349aab414f3
Michael Howard <mikehow@microsoft.com>
2025-10-07 21:24:58 -0500

    Added more comments and cleaned up dynamic memory allocation

--- LestPriv/LestPriv.cpp
+++ LestPriv/LestPriv.cpp
@@ -1,36 +1,35 @@
-// Author Michael Howard (mikehow@microsoft.com)
-// Jan 3rd, 2023
+// Michael Howard (mikehow@microsoft.com)
+// Microsoft Red Team
+// Oct 7th, 2025
+
+// This code demonstrates how to reduce the privileges and integrity level of the current process on Windows.
 
 #include <windows.h>
 #include <sddl.h>
 #include <string>
 #include <vector>
 #include "sal.h"
+#include <memory>
 
 #pragma comment(lib, "advapi32.lib")
 
+const size_t MAX_PRIVILEGE_COUNT = 34;
 
 _Check_return_ bool RemovePrivileges(_In_ const std::vector<std::wstring> &privsToRemove)
 {
-    if (privsToRemove.size() == 0 || privsToRemove.size() >= 24)
-    {
+    if (privsToRemove.size() == 0 || privsToRemove.size() >= MAX_PRIVILEGE_COUNT)
         return false;
-    }
 	
     HANDLE hToken{};
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
-    {
         return false;
-    }
 	
-    // The buffer size calculation is safe from integer overflow because the only data that could be controlled by an attacker is the
-    // number of privs to remove, and this is constrained in the first line of this function
-    TOKEN_PRIVILEGES* pTokenPrivs = (TOKEN_PRIVILEGES*)new char[sizeof(TOKEN_PRIVILEGES) + sizeof(LUID_AND_ATTRIBUTES) * (privsToRemove.size() - 1)];
+    size_t bufferSize = sizeof(TOKEN_PRIVILEGES) + sizeof(LUID_AND_ATTRIBUTES) * (privsToRemove.size() - 1);
+    std::unique_ptr<char[]> buffer(new char[bufferSize]);
+    TOKEN_PRIVILEGES* pTokenPrivs = reinterpret_cast<TOKEN_PRIVILEGES*>(buffer.get());
 
     if (!pTokenPrivs)
-    {
         return false;
-    }
 	
     pTokenPrivs->PrivilegeCount = privsToRemove.size();
 
@@ -47,21 +46,11 @@ _Check_return_ bool RemovePrivileges(_In_ const std::vector<std::wstring> &privs
         pTokenPrivs->Privileges[i].Attributes = SE_PRIVILEGE_REMOVED;
     }
 
-    // See comment about integer overflow above
-    if (!fRes || !AdjustTokenPrivileges(hToken, FALSE, pTokenPrivs, sizeof(TOKEN_PRIVILEGES) + sizeof(LUID_AND_ATTRIBUTES) * privsToRemove.size(), NULL, NULL))
-    {
+    if (!fRes || !AdjustTokenPrivileges(hToken, FALSE, pTokenPrivs, bufferSize, NULL, NULL))
         fRes = false;
-    }
-	
-    if (pTokenPrivs)
-    {
-        delete[] pTokenPrivs;
-    }
 	
     if (hToken)
-    {
         CloseHandle(hToken);
-    }
 	
     return fRes;
 }
@@ -70,9 +59,7 @@ _Check_return_ bool SetLowIntegrityLevel()
 {
     HANDLE hToken{};
     if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &hToken))
-    {
         return false;
-    }
 
     TOKEN_MANDATORY_LABEL integrityLabel{};
     SID sidLowIntegrity = {
@@ -85,36 +72,27 @@ _Check_return_ bool SetLowIntegrityLevel()
     bool fRes = true;
     DWORD dwTokenSize = 0;
     if (!GetTokenInformation(hToken, TokenIntegrityLevel, reinterpret_cast<LPVOID>(&integrityLabel), dwTokenSize, &dwTokenSize) && GetLastError() != ERROR_INSUFFICIENT_BUFFER)
-    {
         fRes = false;
-    }
 	
     if (!fRes || !SetTokenInformation(hToken, TokenIntegrityLevel, reinterpret_cast<LPVOID>(&integrityLabel), dwTokenSize))
-    {
         fRes = false;
-    }
 
     if (hToken)
-    {
         CloseHandle(hToken);
-    }
 
     return fRes;
 }
 
-
-
 int main() {
 
-    // From https://learn.microsoft.com/en-us/windows/win32/secauthz/privilege-constants
+    // From https://learn.microsoft.com/en-us/windows/win32/secauthz/privilege-constants and winnt.h
     const std::vector<std::wstring> privs{ SE_BACKUP_NAME, SE_RESTORE_NAME, SE_TCB_NAME, SE_TAKE_OWNERSHIP_NAME, 
                                            SE_DEBUG_NAME, SE_IMPERSONATE_NAME,  SE_CREATE_GLOBAL_NAME, SE_CREATE_TOKEN_NAME, 
                                            SE_SECURITY_NAME, SE_RELABEL_NAME, SE_LOAD_DRIVER_NAME, SE_SYSTEMTIME_NAME};
     
-    // reduce privilge and integrity level
-    if (RemovePrivileges(privs) == false || SetLowIntegrityLevel() == false) 
-    {
-        printf("Failed to remove privileges or set low integrity level");
+    // reduce privilege and integrity level
+    if (RemovePrivileges(privs) == false || SetLowIntegrityLevel() == false) {
+        printf("Failed to remove privileges or set low integrity level, err==%d", GetLastError());
         return false;
     }
 
